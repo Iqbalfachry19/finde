101a695f628f6213953c3e44f0eea8cc25407bbf
iqbal <ifachry@rocketmail.com>
2024-06-12 08:51:16 +0700

    hi

--- next.config.mjs
+++ next.config.mjs
@@ -12,9 +12,9 @@ const nextConfig = {
         source: '/admin/get_laptop.php',
         destination: 'https://carilaptops.cloud/get_laptop.php' // Proxy to Backend
       },
-        {
-        source: '/admin/assets/logo1.png',
-        destination: 'https://finderlaptop.vercel.app/assets/logo1.png' // Proxy to Backend
+         {
+        source: '/admin/assets/:path*', // Matches all paths under /admin/assets/
+        destination: 'https://finderlaptop.vercel.app/assets/:path*' // Proxy to Backend
       }
     ];
   }
