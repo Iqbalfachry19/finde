84c8e7e08fb3fd36cdce9ce90cb61876fd33f785
iqbal <ifachry@rocketmail.com>
2024-07-05 01:51:16 +0700

    hi

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="bg-[#181825] text-[#cdd6f4] p-4 text-center">
+    <footer className="bg-[#181825] dark:bg-slate-600 text-[#cdd6f4] p-4 text-center">
       <p>&copy; 2024 Finde</p>
     </footer>
   );
